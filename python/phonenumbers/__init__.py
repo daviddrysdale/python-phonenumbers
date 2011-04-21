067f9367fcfda3abc664978d2ff8193685b1766b
David Drysdale <dmd@lurklurk.org>
2011-04-20 21:09:01 -0700

    Add examples to docstring

--- python/phonenumbers/__init__.py
+++ python/phonenumbers/__init__.py
@@ -1,4 +1,55 @@
-"""Python phone number parsing and formatting library"""
+"""Python phone number parsing and formatting library
+
+Examples of use:
+
+>>> import phonenumbers
+>>> x = phonenumbers.parse("+442083661177", None)
+>>> print x
+Country Code: 44 National Number: 2083661177 Leading Zero: False
+>>> phonenumbers.format_number(x, phonenumbers.PhoneNumberFormat.NATIONAL)
+u'020 8366 1177'
+>>> phonenumbers.format_number(x, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+u'+44 20 8366 1177'
+>>> phonenumbers.format_number(x, phonenumbers.PhoneNumberFormat.E164)
+u'+442083661177'
+>>> y = phonenumbers.parse("020 8366 1177", "GB")
+>>> print y
+Country Code: 44 National Number: 2083661177 Leading Zero: False
+>>> x == y
+True
+>>>
+>>> formatter = phonenumbers.AsYouTypeFormatter("US")
+>>> print formatter.input_digit("6")
+6
+>>> print formatter.input_digit("5")
+65
+>>> print formatter.input_digit("0")
+(650
+>>> print formatter.input_digit("2")
+(650) 2
+>>> print formatter.input_digit("5")
+(650) 25
+>>> print formatter.input_digit("3")
+(650) 253
+>>> print formatter.input_digit("2")
+650-2532
+>>> print formatter.input_digit("2")
+(650) 253-22
+>>> print formatter.input_digit("2")
+(650) 253-222
+>>> print formatter.input_digit("2")
+(650) 253-2222
+>>>
+>>> text = "Call me at 510-748-8230 some time after 9:30."
+>>>
+>>> text = "Call me at 510-748-8230 if it's before 9:30, or on 703-4800500 after 10am."
+>>> for match in phonenumbers.PhoneNumberMatcher(text, "US"):
+...     print match
+... 
+PhoneNumberMatch [11,23) 510-748-8230
+PhoneNumberMatch [51,62) 703-4800500
+
+"""
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
