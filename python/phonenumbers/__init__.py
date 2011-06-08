506bf26326fda311b4e3cccbe6f3d9a67950a3e9
David Drysdale <dmd@lurklurk.org>
2011-06-08 10:09:07 -0700

    Bump version number to 3.5b2 to include latest fix

--- python/phonenumbers/__init__.py
+++ python/phonenumbers/__init__.py
@@ -74,7 +74,7 @@ PhoneNumberMatch [51,62) 703-4800500
 
 # Version number is taken from the upstream libphonenumber version
 # together with an indication of the version of the Python-specific code.
-__version__ = "3.5b1"
+__version__ = "3.5b2"
 
 # Data class definitions
 from phonenumber import PhoneNumber, CountryCodeSource
