9f362e2b7a53e62ca763979597c78053db172c95
David Drysdale <dmd@lurklurk.org>
2011-07-07 18:26:18 +0100

    Generated Python files from geocoding metadata

---
+++ python/tests/testgeodata/__init__.py
@@ -0,0 +1,45 @@
+"""Geocoding data, mapping each prefix to a dict of locale:locationname.
+
+Auto-generated file, do not edit by hand.
+"""
+
+# Copyright (C) 2011-2011 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+GEOCODE_DATA = {
+  '8255': {'en': u'Gyeongnam', 'ko': u'\uacbd\ub0a8'},
+  '8232': {'en': u'Incheon', 'ko': u'\uc778\ucc9c'},
+  '8231': {'en': u'Gyeonggi', 'ko': u'\uacbd\uae30'},
+  '8251': {'en': u'Busan', 'ko': u'\ubd80\uc0b0'},
+  '8253': {'en': u'Daegu', 'ko': u'\ub300\uad6c'},
+  '8252': {'en': u'Ulsan', 'ko': u'\uc6b8\uc0b0'},
+  '1650960': {'en': u'Mountain View, CA'},
+  '1201': {'en': u'NJ'},
+  '8264': {'en': u'Jeju', 'ko': u'\uc81c\uc8fc'},
+  '1989': {'en': u'MA'},
+  '1212812': {'en': u'New York, NY'},
+  '1650': {'en': u'CA'},
+  '1212': {'en': u'NY'},
+  '8254': {'en': u'Gyeongbuk', 'ko': u'\uacbd\ubd81'},
+  '8233': {'en': u'Gangwon', 'ko': u'\uac15\uc6d0'},
+  '8242': {'en': u'Daejeon', 'ko': u'\ub300\uc804'},
+  '8243': {'en': u'Chungbuk', 'ko': u'\ucda9\ubd81'},
+  '8241': {'en': u'Chungnam', 'ko': u'\ucda9\ub0a8'},
+  '8261': {'en': u'Jeonnam', 'ko': u'\uc804\ub0a8'},
+  '8262': {'en': u'Gwangju', 'ko': u'\uad11\uc8fc'},
+  '8263': {'en': u'Jeonbuk', 'ko': u'\uc804\ubd81'},
+  '1617423': {'en': u'Boston, MA'},
+  '822': {'en': u'Seoul', 'ko': u'\uc11c\uc6b8'},
+}
+GEOCODE_LONGEST_PREFIX = 7
