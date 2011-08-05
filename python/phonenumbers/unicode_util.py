4e84814cc36819437da9fb2383fd9c66ec0926b1
David Drysdale <dmd@lurklurk.org>
2011-08-05 08:10:53 +0100

    Remember to actually ellipsis-out the hash value in doctest.
    Wrapped in str() because an ellipsis at the start of a line looks like a continuation.

--- python/phonenumbers/unicode_util.py
+++ python/phonenumbers/unicode_util.py
@@ -59,8 +59,8 @@ True
 -1
 >>> unicode_util.digit(u3, -1)
 2
->>> hash(b3)  # doctest: +ELLIPSIS
-3736082139298884806
+>>> str(hash(b3))  # doctest: +ELLIPSIS
+'...'
 """
 import bisect
 import unicodedata  # Python 2.5 onward
