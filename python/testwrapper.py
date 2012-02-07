30700e582f1d5e0c34144881faf080e678d4f3f3
David Drysdale <dmd@lurklurk.org>
2012-02-07 09:21:32 +0000

    Fold run of package doctest into testwrapper.py

--- python/testwrapper.py
+++ python/testwrapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import doctest
 import unittest
+import phonenumbers
 from phonenumbers import util
 from phonenumbers import re_util
 from phonenumbers import unicode_util
@@ -8,6 +9,7 @@ from phonenumbers import geocoder
 from tests import *
 
 if __name__ == '__main__':
+    doctest.testmod(phonenumbers)
     doctest.testmod(util)
     doctest.testmod(re_util)
     doctest.testmod(unicode_util)
