653657c203dc8b5e16a83eab882a16a6966f4647
David Drysdale <dmd@lurklurk.org>
2012-02-07 09:35:10 +0000

    Move doctests into testwrapper.py
    This is roughly a cherry-pick of commit 68160e34
    and commit 30700e58.

--- python/testwrapper.py
+++ python/testwrapper.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+import doctest
 import unittest
+import phonenumbers
+from phonenumbers import util
+from phonenumbers import re_util
+from phonenumbers import unicode_util
+from phonenumbers import geocoder
 from tests import *
 
 if __name__ == '__main__':
+    doctest.testmod(phonenumbers)
+    doctest.testmod(util)
+    doctest.testmod(re_util)
+    doctest.testmod(unicode_util)
+    doctest.testmod(geocoder)
     unittest.main()
