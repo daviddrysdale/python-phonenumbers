f876ec5ce60ec83d85e8bb7036a3b0fe30069bf3
David Drysdale <dmd@lurklurk.org>
2012-05-30 10:44:53 +0100

    Generated Python files for metadata

--- python/phonenumbers/data/region_AN.py
+++ python/phonenumbers/data/region_AN.py
@@ -3,8 +3,8 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_AN = PhoneMetadata(id='AN', country_code=599, international_prefix='00',
     general_desc=PhoneNumberDesc(national_number_pattern='5\\d{6}', possible_number_pattern='\\d{7}'),
-    fixed_line=PhoneNumberDesc(national_number_pattern='5(?:4\\d|8[239])\\d{4}', possible_number_pattern='\\d{7}', example_number='5451234'),
-    mobile=PhoneNumberDesc(national_number_pattern='5(?:1[01]|2[0-7]|5\\d|8[016-8])\\d{4}', possible_number_pattern='\\d{7}', example_number='5101234'),
+    fixed_line=PhoneNumberDesc(national_number_pattern='5(?:4[2-8]|8[239])\\d{4}', possible_number_pattern='\\d{7}', example_number='5451234'),
+    mobile=PhoneNumberDesc(national_number_pattern='5(?:1[02]|2\\d|5[0-79]|8[016-8])\\d{4}', possible_number_pattern='\\d{7}', example_number='5101234'),
     toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     premium_rate=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
