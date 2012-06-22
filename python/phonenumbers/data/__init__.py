8471ccfece97af821db49798919e8b442c01cc16
David Drysdale <dmd@lurklurk.org>
2012-06-22 12:57:04 +0100

    Generated Python files for metadata

--- python/phonenumbers/data/__init__.py
+++ python/phonenumbers/data/__init__.py
@@ -18,6 +18,7 @@ from .region_808 import PHONE_METADATA_808
 from .region_870 import PHONE_METADATA_870
 from .region_878 import PHONE_METADATA_878
 from .region_881 import PHONE_METADATA_881
+from .region_882 import PHONE_METADATA_882
 from .region_883 import PHONE_METADATA_883
 from .region_888 import PHONE_METADATA_888
 from .region_979 import PHONE_METADATA_979
@@ -458,6 +459,7 @@ _COUNTRY_CODE_TO_REGION_CODE = {
     878: ("001",),
     880: ("BD",),
     881: ("001",),
+    882: ("001",),
     883: ("001",),
     886: ("TW",),
     888: ("001",),
