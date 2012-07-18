08dd30f8f35cbe0661ee1f89e1282f77fce54d9c
David Drysdale <dmd@lurklurk.org>
2012-07-18 20:10:28 +0100

    Generated Python files for metadata

--- python/phonenumbers/data/region_GA.py
+++ python/phonenumbers/data/region_GA.py
@@ -2,9 +2,9 @@
 from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_GA = PhoneMetadata(id='GA', country_code=241, international_prefix='00',
-    general_desc=PhoneNumberDesc(national_number_pattern='[12]\\d{6}|0\\d{7}', possible_number_pattern='\\d{6,8}'),
-    fixed_line=PhoneNumberDesc(national_number_pattern='(?:1(?:4[4-8]|7\\d)|2(?:4[02]|5[04-689]|6[024-79]|8[236]|9[02368]))\\d{4}', possible_number_pattern='\\d{6,7}', example_number='1441234'),
-    mobile=PhoneNumberDesc(national_number_pattern='0(?:5(?:0[89]|3[0-4]|8[0-26]|9[238])|6(?:0[3-7]|1[01]|2[0-7]|6[0-589]|71|83|9[57])|7(?:1[2-5]|2[89]|3[35-9]|4[01]|5[0-47-9]|[67]\\d|8[457-9]|9[0146]))\\d{4}', possible_number_pattern='\\d{8}', example_number='06031234'),
+    general_desc=PhoneNumberDesc(national_number_pattern='[1-7]\\d{6}', possible_number_pattern='\\d{7}'),
+    fixed_line=PhoneNumberDesc(national_number_pattern='1\\d{6}', possible_number_pattern='\\d{7}', example_number='1441234'),
+    mobile=PhoneNumberDesc(national_number_pattern='[2-7]\\d{6}', possible_number_pattern='\\d{7}', example_number='6031234'),
     toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     premium_rate=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
@@ -17,6 +17,4 @@ PHONE_METADATA_GA = PhoneMetadata(id='GA', country_code=241, international_prefi
     no_international_dialling=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     national_prefix='0',
     national_prefix_for_parsing='0',
-    number_format=[NumberFormat(pattern='(\\d)(\\d{2})(\\d{2})(\\d{2})', format=u'\\1 \\2 \\3 \\4', leading_digits_pattern=['[12]'], national_prefix_formatting_rule=u'0\\1'),
-        NumberFormat(pattern='(0\\d)(\\d{2})(\\d{2})(\\d{2})', format=u'\\1 \\2 \\3 \\4', leading_digits_pattern=['0'])],
-    leading_zero_possible=True)
+    number_format=[NumberFormat(pattern='(\\d)(\\d{2})(\\d{2})(\\d{2})', format=u'\\1 \\2 \\3 \\4', national_prefix_formatting_rule=u'0\\1')])
--- python/phonenumbers/data/region_IL.py
+++ python/phonenumbers/data/region_IL.py
@@ -1,15 +1,15 @@
 """Auto-generated file, do not edit by hand. IL metadata"""
 from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
-PHONE_METADATA_IL = PhoneMetadata(id='IL', country_code=972, international_prefix='0(?:0|1[2-48])',
+PHONE_METADATA_IL = PhoneMetadata(id='IL', country_code=972, international_prefix='0(?:0|1[2-9])',
     general_desc=PhoneNumberDesc(national_number_pattern='[17]\\d{6,9}|[2-589]\\d{3}(?:\\d{3,6})?|6\\d{3}', possible_number_pattern='\\d{4,10}'),
-    fixed_line=PhoneNumberDesc(national_number_pattern='(?:[2-489]|7[2-46-8])\\d{7}', possible_number_pattern='\\d{7,9}', example_number='21234567'),
-    mobile=PhoneNumberDesc(national_number_pattern='5(?:[02346-9]\\d{2}|5(?:22|33|44|5[58]|66|77|88))\\d{5}', possible_number_pattern='\\d{9}', example_number='501234567'),
+    fixed_line=PhoneNumberDesc(national_number_pattern='[2-489]\\d{7}', possible_number_pattern='\\d{7,8}', example_number='21234567'),
+    mobile=PhoneNumberDesc(national_number_pattern='5(?:[02347-9]\\d{2}|5(?:2[23]|3[34]|4[45]|5[5689]|6[67]|7[78]|8[89])|6[2-9]\\d)\\d{5}', possible_number_pattern='\\d{9}', example_number='501234567'),
     toll_free=PhoneNumberDesc(national_number_pattern='1(?:80[019]\\d{3}|255)\\d{3}', possible_number_pattern='\\d{7,10}', example_number='1800123456'),
-    premium_rate=PhoneNumberDesc(national_number_pattern='1(?:212|(?:919|200)\\d{2})\\d{4}', possible_number_pattern='\\d{8,10}', example_number='1919123456'),
+    premium_rate=PhoneNumberDesc(national_number_pattern='1(?:212|(?:9(?:0[01]|19)|200)\\d{2})\\d{4}', possible_number_pattern='\\d{8,10}', example_number='1919123456'),
     shared_cost=PhoneNumberDesc(national_number_pattern='1700\\d{6}', possible_number_pattern='\\d{10}', example_number='1700123456'),
     personal_number=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
-    voip=PhoneNumberDesc(national_number_pattern='77\\d{7}', possible_number_pattern='\\d{9}', example_number='771234567'),
+    voip=PhoneNumberDesc(national_number_pattern='7(?:2[23]\\d|3[237]\\d|47\\d|6(?:5\\d|88)|7\\d{2}|8(?:33|55|77|81))\\d{5}', possible_number_pattern='\\d{9}', example_number='771234567'),
     pager=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     uan=PhoneNumberDesc(national_number_pattern='[2-689]\\d{3}', possible_number_pattern='\\d{4}', example_number='2250'),
     emergency=PhoneNumberDesc(national_number_pattern='1(?:0[012]|12)', possible_number_pattern='\\d{3}', example_number='112'),
