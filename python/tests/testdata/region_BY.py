356ba954eec7601a01eb66e3affe700da4dc09df
David Drysdale <dmd@lurklurk.org>
2012-09-08 08:26:05 +0100

    Generated Python files for metadata

---
+++ python/tests/testdata/region_BY.py
@@ -0,0 +1,22 @@
+"""Auto-generated file, do not edit by hand. BY metadata"""
+from phonenumbers.phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
+
+PHONE_METADATA_BY = PhoneMetadata(id='BY', country_code=375, international_prefix='810',
+    general_desc=PhoneNumberDesc(national_number_pattern='[1-9]\\d{5}', possible_number_pattern='\\d{6}'),
+    fixed_line=PhoneNumberDesc(national_number_pattern='[1-9]\\d{5}', possible_number_pattern='\\d{6}', example_number='112345'),
+    mobile=PhoneNumberDesc(national_number_pattern='[1-9]\\d{5}', possible_number_pattern='\\d{6}'),
+    toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    premium_rate=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    personal_number=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    voip=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    pager=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    uan=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    emergency=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    voicemail=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    no_international_dialling=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    national_prefix='8',
+    national_prefix_for_parsing='80?|99999',
+    number_format=[NumberFormat(pattern='(\\d{4})', format=u'\\1', leading_digits_pattern=['[1-8]'], national_prefix_formatting_rule=u'8 \\1'),
+        NumberFormat(pattern='(\\d{2})(\\d{3})', format=u'\\1 \\2', leading_digits_pattern=['[1-8]'], national_prefix_formatting_rule=u'8\\1'),
+        NumberFormat(pattern='(\\d{3})(\\d{3})', format=u'\\1 \\2', leading_digits_pattern=['[1-8]'], national_prefix_formatting_rule=u'8 \\1')])
--- python/tests/testdata/region_US.py
+++ python/tests/testdata/region_US.py
@@ -18,7 +18,7 @@ PHONE_METADATA_US = PhoneMetadata(id='US', country_code=1, international_prefix=
     national_prefix='1',
     preferred_extn_prefix=' extn. ',
     national_prefix_for_parsing='1',
-    number_format=[NumberFormat(pattern='(\\d{3})(\\d{4})', format=u'\\1 \\2'),
-        NumberFormat(pattern='(\\d{3})(\\d{3})(\\d{4})', format=u'\\1 \\2 \\3')],
+    number_format=[NumberFormat(pattern='(\\d{3})(\\d{4})', format=u'\\1 \\2', national_prefix_optional_when_formatting=True),
+        NumberFormat(pattern='(\\d{3})(\\d{3})(\\d{4})', format=u'\\1 \\2 \\3', national_prefix_optional_when_formatting=True)],
     intl_number_format=[NumberFormat(pattern='(\\d{3})(\\d{3})(\\d{4})', format=u'\\1 \\2 \\3')],
     main_country_for_code=True)
