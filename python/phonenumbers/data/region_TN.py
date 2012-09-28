a20a6aed2e467aa496186c7a999b6f7c38d4dde6
David Drysdale <dmd@lurklurk.org>
2012-09-28 07:11:25 +0100

    Generated Python files for metadata

--- python/phonenumbers/data/region_TN.py
+++ python/phonenumbers/data/region_TN.py
@@ -4,7 +4,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 PHONE_METADATA_TN = PhoneMetadata(id='TN', country_code=216, international_prefix='00',
     general_desc=PhoneNumberDesc(national_number_pattern='[2-57-9]\\d{7}', possible_number_pattern='\\d{8}'),
     fixed_line=PhoneNumberDesc(national_number_pattern='(?:3[012]|7\\d)\\d{6}', possible_number_pattern='\\d{8}', example_number='71234567'),
-    mobile=PhoneNumberDesc(national_number_pattern='(?:[259]\\d|4[01])\\d{6}', possible_number_pattern='\\d{8}', example_number='20123456'),
+    mobile=PhoneNumberDesc(national_number_pattern='(?:[259]\\d|4[0-2])\\d{6}', possible_number_pattern='\\d{8}', example_number='20123456'),
     toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     premium_rate=PhoneNumberDesc(national_number_pattern='8[0128]\\d{6}', possible_number_pattern='\\d{8}', example_number='80123456'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
