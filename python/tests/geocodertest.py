792f3a381aeba56ce92399776a7e86130bbd42e6
David Drysdale <dmd@lurklurk.org>
2013-10-05 16:50:18 +0100

    Make area_description_for_number generic.
    It looks up a number in a prefix-indexed set of
    data, and is not geocoding-data specific.

--- python/tests/geocodertest.py
+++ python/tests/geocodertest.py
@@ -23,7 +23,7 @@ from phonenumbers import PhoneNumber, FrozenPhoneNumber
 from phonenumbers import geocoder
 from phonenumbers import description_for_number, country_name_for_number
 from phonenumbers import description_for_valid_number
-from phonenumbers.geocoder import _area_description_for_number
+from phonenumbers.prefix import prefix_description_for_number
 
 # Allow override library geocoding metadata with the test metadata.
 REAL_GEOCODE_DATA = geocoder.GEOCODE_DATA
@@ -166,7 +166,7 @@ class PhoneNumberGeocoderTest(unittest.TestCase):
         invalid_number = PhoneNumber(country_code=210, national_number=123456L)
         self.assertEqual("", country_name_for_number(invalid_number, "en"))
         # Ensure we exercise all public entrypoints directly
-        self.assertEqual("CA", _area_description_for_number(US_NUMBER1, "en"))
+        self.assertEqual("CA", prefix_description_for_number(TEST_GEOCODE_DATA, TEST_GEOCODE_LONGEST_PREFIX, US_NUMBER1, "en"))
         self.assertEqual("CA", description_for_valid_number(US_NUMBER1, "en"))
         self.assertEqual("", description_for_valid_number(US_INVALID_NUMBER, "en"))
         # Add in some script and region specific fictional names
