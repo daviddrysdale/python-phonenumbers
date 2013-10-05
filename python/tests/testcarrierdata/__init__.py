01c67e0a98edd7d56328b08e6f6c149d90e8c725
David Drysdale <dmd@lurklurk.org>
2013-10-05 15:54:10 +0100

    Generate carrierdata Python files

---
+++ python/tests/testcarrierdata/__init__.py
@@ -0,0 +1,29 @@
+"""Per-prefix data, mapping each prefix to a dict of locale:name.
+
+Auto-generated file, do not edit by hand.
+"""
+
+# Copyright (C) 2011-2013 The Libphonenumber Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CARRIER_DATA = {
+ '1650212':{'en': u'US carrier'},
+ '1650213':{'en': u'US carrier2'},
+ '244262':{'en': u'Angolan fixed line carrier'},
+ '244917':{'en': u'Angolan carrier'},
+ '4411':{'en': u'British fixed line carrier'},
+ '4473':{'en': u'British carrier', 'sv': u'Brittisk operat\xf6r'},
+ '44760':{'en': u'British pager'},
+}
+CARRIER_LONGEST_PREFIX = 7
