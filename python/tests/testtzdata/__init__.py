695b71e28ae03fa65362cdd934995bc19e1172e5
David Drysdale <dmd@lurklurk.org>
2013-10-10 12:02:11 +0100

    Generate Python files for timezone metadata

---
+++ python/tests/testtzdata/__init__.py
@@ -0,0 +1,32 @@
+"""Per-prefix data, mapping each prefix to a name.
+
+Auto-generated file, do not edit by hand.
+"""
+
+# Copyright (C) 2011-2013 The Libphonenumber Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TIMEZONE_DATA = {
+ '1':(u'America/New_York', u'America/Chicago', u'America/Winnipeg', u'America/Los_Angeles'),
+ '1201':(u'America/New_York',),
+ '1212812':(u'America/New_York',),
+ '1234':(u'America/New_York',),
+ '1604':(u'America/Winnipeg',),
+ '1617423':(u'America/Chicago',),
+ '1650960':(u'America/Los_Angeles',),
+ '1989':(u'Ameriac/Los_Angeles',),
+ '612':(u'Australia/Sydney',),
+ '82':(u'Asia/Seoul',),
+}
+TIMEZONE_LONGEST_PREFIX = 7
