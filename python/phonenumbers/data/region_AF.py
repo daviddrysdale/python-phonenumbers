b74e0c7d6ed2bcfc41efc3b77b13c9e1143422fd
David Drysdale <dmd@lurklurk.org>
2015-05-29 09:32:53 +0100

    Generated Python files for metadata

--- python/phonenumbers/data/region_AF.py
+++ python/phonenumbers/data/region_AF.py
@@ -4,7 +4,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 PHONE_METADATA_AF = PhoneMetadata(id='AF', country_code=93, international_prefix='00',
     general_desc=PhoneNumberDesc(national_number_pattern='[2-7]\\d{8}', possible_number_pattern='\\d{7,9}'),
     fixed_line=PhoneNumberDesc(national_number_pattern='(?:[25][0-8]|[34][0-4]|6[0-5])[2-9]\\d{6}', possible_number_pattern='\\d{7,9}', example_number='234567890'),
-    mobile=PhoneNumberDesc(national_number_pattern='7(?:[05-9]\\d{7}|29\\d{6})', possible_number_pattern='\\d{9}', example_number='701234567'),
+    mobile=PhoneNumberDesc(national_number_pattern='7(?:[014-9]\\d{7}|2[89]\\d{6})', possible_number_pattern='\\d{9}', example_number='701234567'),
     toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     premium_rate=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
@@ -16,5 +16,4 @@ PHONE_METADATA_AF = PhoneMetadata(id='AF', country_code=93, international_prefix
     no_international_dialling=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     national_prefix='0',
     national_prefix_for_parsing='0',
-    number_format=[NumberFormat(pattern='([2-7]\\d)(\\d{3})(\\d{4})', format='\\1 \\2 \\3', leading_digits_pattern=['[2-6]|7[013-9]'], national_prefix_formatting_rule='0\\1'),
-        NumberFormat(pattern='(729)(\\d{3})(\\d{3})', format='\\1 \\2 \\3', leading_digits_pattern=['729'], national_prefix_formatting_rule='0\\1')])
+    number_format=[NumberFormat(pattern='([2-7]\\d)(\\d{3})(\\d{4})', format='\\1 \\2 \\3', leading_digits_pattern=['[2-7]'], national_prefix_formatting_rule='0\\1')])
