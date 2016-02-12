46f3ed652cf62a09bdd2c28cf00fdd31c2cc5171
David Drysdale <dmd@lurklurk.org>
2016-02-12 09:01:17 +0000

    Generated Python files for metadata

--- python/phonenumbers/data/region_BB.py
+++ python/phonenumbers/data/region_BB.py
@@ -8,7 +8,7 @@ PHONE_METADATA_BB = PhoneMetadata(id='BB', country_code=1, international_prefix=
     toll_free=PhoneNumberDesc(national_number_pattern='8(?:00|44|55|66|77|88)[2-9]\\d{6}', possible_number_pattern='\\d{10}', example_number='8002123456'),
     premium_rate=PhoneNumberDesc(national_number_pattern='900\\d{7}|246976\\d{4}', possible_number_pattern='\\d{10}', example_number='9002123456'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
-    personal_number=PhoneNumberDesc(national_number_pattern='5(?:00|33|44|66|77)[2-9]\\d{3}', possible_number_pattern='\\d{10}', example_number='5002345678'),
+    personal_number=PhoneNumberDesc(national_number_pattern='5(?:00|33|44|66|77|88)[2-9]\\d{6}', possible_number_pattern='\\d{10}', example_number='5002345678'),
     voip=PhoneNumberDesc(national_number_pattern='24631\\d{5}', possible_number_pattern='\\d{10}', example_number='2463101234'),
     pager=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     uan=PhoneNumberDesc(national_number_pattern='246(?:292|41[7-9]|43[01])\\d{4}', possible_number_pattern='\\d{10}', example_number='2464301234'),
--- python/phonenumbers/data/region_SK.py
+++ python/phonenumbers/data/region_SK.py
@@ -2,8 +2,8 @@
 from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_SK = PhoneMetadata(id='SK', country_code=421, international_prefix='00',
-    general_desc=PhoneNumberDesc(national_number_pattern='(?:[2-68]\\d{8}|9\\d{6,8})', possible_number_pattern='\\d{7,9}'),
-    fixed_line=PhoneNumberDesc(national_number_pattern='[2-5]\\d{8}', possible_number_pattern='\\d{9}', example_number='212345678'),
+    general_desc=PhoneNumberDesc(national_number_pattern='(?:[2-68]\\d{5,8}|9\\d{6,8})', possible_number_pattern='\\d{6,9}'),
+    fixed_line=PhoneNumberDesc(national_number_pattern='2(?:16\\d{3,4}|\\d{8})|[3-5](?:[1-8]16\\d{2,3}|\\d{8})', possible_number_pattern='\\d{6,9}', example_number='212345678'),
     mobile=PhoneNumberDesc(national_number_pattern='9(?:0[1-8]|1[0-24-9]|4[0489]|50)\\d{6}', possible_number_pattern='\\d{9}', example_number='912123456'),
     toll_free=PhoneNumberDesc(national_number_pattern='800\\d{6}', possible_number_pattern='\\d{9}', example_number='800123456'),
     premium_rate=PhoneNumberDesc(national_number_pattern='9(?:[78]\\d{7}|00\\d{6})', possible_number_pattern='\\d{9}', example_number='900123456'),
@@ -16,7 +16,9 @@ PHONE_METADATA_SK = PhoneMetadata(id='SK', country_code=421, international_prefi
     no_international_dialling=PhoneNumberDesc(national_number_pattern='(?:602|8(?:00|[5-9]\\d)|9(?:00|[78]\\d))\\d{6}|9090\\d{3}', possible_number_pattern='\\d{7,9}', example_number='800123456'),
     national_prefix='0',
     national_prefix_for_parsing='0',
-    number_format=[NumberFormat(pattern='(2)(\\d{3})(\\d{3})(\\d{2})', format='\\1/\\2 \\3 \\4', leading_digits_pattern=['2'], national_prefix_formatting_rule='0\\1'),
+    number_format=[NumberFormat(pattern='(2)(16)(\\d{3,4})', format='\\1 \\2 \\3', leading_digits_pattern=['216'], national_prefix_formatting_rule='0\\1'),
+        NumberFormat(pattern='([3-5]\\d)(16)(\\d{2,3})', format='\\1 \\2 \\3', leading_digits_pattern=['[3-5]'], national_prefix_formatting_rule='0\\1'),
+        NumberFormat(pattern='(2)(\\d{3})(\\d{3})(\\d{2})', format='\\1/\\2 \\3 \\4', leading_digits_pattern=['2'], national_prefix_formatting_rule='0\\1'),
         NumberFormat(pattern='([3-5]\\d)(\\d{3})(\\d{2})(\\d{2})', format='\\1/\\2 \\3 \\4', leading_digits_pattern=['[3-5]'], national_prefix_formatting_rule='0\\1'),
         NumberFormat(pattern='([689]\\d{2})(\\d{3})(\\d{3})', format='\\1 \\2 \\3', leading_digits_pattern=['[689]'], national_prefix_formatting_rule='0\\1'),
         NumberFormat(pattern='(9090)(\\d{3})', format='\\1 \\2', leading_digits_pattern=['9090'], national_prefix_formatting_rule='0\\1')],
