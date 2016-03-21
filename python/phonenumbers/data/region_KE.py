135de6e4123fd777eb192d1eb77d7118978ef735
David Drysdale <dmd@lurklurk.org>
2016-03-21 17:58:37 +0000

    Generated Python files for metadata

--- python/phonenumbers/data/region_KE.py
+++ python/phonenumbers/data/region_KE.py
@@ -4,7 +4,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 PHONE_METADATA_KE = PhoneMetadata(id='KE', country_code=254, international_prefix='000',
     general_desc=PhoneNumberDesc(national_number_pattern='20\\d{6,7}|[4-9]\\d{6,9}', possible_number_pattern='\\d{7,10}'),
     fixed_line=PhoneNumberDesc(national_number_pattern='20\\d{6,7}|4(?:[0136]\\d{7}|[245]\\d{5,7})|5(?:[08]\\d{7}|[1-79]\\d{5,7})|6(?:[01457-9]\\d{5,7}|[26]\\d{7})', possible_number_pattern='\\d{7,9}', example_number='202012345'),
-    mobile=PhoneNumberDesc(national_number_pattern='7(?:[0-36]\\d|5[0-6]|7[0-5]|8[0-25-9]|9[0-4])\\d{6}', possible_number_pattern='\\d{9}', example_number='712123456'),
+    mobile=PhoneNumberDesc(national_number_pattern='7(?:[0-36]\\d|5[0-6]|7[0-5]|8[0-25-9]|9[0-7])\\d{6}', possible_number_pattern='\\d{9}', example_number='712123456'),
     toll_free=PhoneNumberDesc(national_number_pattern='800[24-8]\\d{5,6}', possible_number_pattern='\\d{9,10}', example_number='800223456'),
     premium_rate=PhoneNumberDesc(national_number_pattern='900[02-9]\\d{5}', possible_number_pattern='\\d{9}', example_number='900223456'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
--- python/phonenumbers/data/region_SO.py
+++ python/phonenumbers/data/region_SO.py
@@ -4,7 +4,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 PHONE_METADATA_SO = PhoneMetadata(id='SO', country_code=252, international_prefix='00',
     general_desc=PhoneNumberDesc(national_number_pattern='[1-79]\\d{6,8}', possible_number_pattern='\\d{7,9}'),
     fixed_line=PhoneNumberDesc(national_number_pattern='(?:1\\d|2[0-79]|3[0-46-8]|4[0-7]|59)\\d{5}', possible_number_pattern='\\d{7}', example_number='4012345'),
-    mobile=PhoneNumberDesc(national_number_pattern='(?:15\\d|2(?:4\\d|8)|6[1-35-9]?\\d{2}|7(?:[1-8]\\d|99?\\d)|9(?:07|[2-9])\\d)\\d{5}', possible_number_pattern='\\d{7,9}', example_number='71123456'),
+    mobile=PhoneNumberDesc(national_number_pattern='(?:15\\d|2(?:4\\d|8)|6[1-35-9]?\\d{2}|7(?:[1-8]\\d|99?\\d)|9(?:0[67]|[2-9])\\d)\\d{5}', possible_number_pattern='\\d{7,9}', example_number='71123456'),
     toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     premium_rate=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
