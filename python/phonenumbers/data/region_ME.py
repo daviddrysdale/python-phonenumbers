3a6356801c6cc255a24885d2a07a1f3c97c9fcb7
David Drysdale <dmd@lurklurk.org>
2016-08-12 17:21:51 +0100

    Generated Python files for metadata

--- python/phonenumbers/data/region_ME.py
+++ python/phonenumbers/data/region_ME.py
@@ -4,7 +4,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 PHONE_METADATA_ME = PhoneMetadata(id='ME', country_code=382, international_prefix='00',
     general_desc=PhoneNumberDesc(national_number_pattern='[2-9]\\d{7,8}', possible_number_pattern='\\d{6,9}'),
     fixed_line=PhoneNumberDesc(national_number_pattern='(?:20[2-8]|3(?:0[2-7]|[12][35-7]|3[4-7])|4(?:0[2367]|1[267])|5(?:0[467]|1[267]|2[367]))\\d{5}', possible_number_pattern='\\d{6,8}', example_number='30234567'),
-    mobile=PhoneNumberDesc(national_number_pattern='6(?:00\\d|32\\d|[89]\\d{2}|61\\d|7(?:[0-8]\\d|9(?:[3-9]|[0-2]\\d)))\\d{4}', possible_number_pattern='\\d{8,9}', example_number='67622901'),
+    mobile=PhoneNumberDesc(national_number_pattern='6(?:00\\d|3[24]\\d|61\\d|7(?:[0-8]\\d|9(?:[3-9]|[0-2]\\d))|[89]\\d{2})\\d{4}', possible_number_pattern='\\d{8,9}', example_number='67622901'),
     toll_free=PhoneNumberDesc(national_number_pattern='80\\d{6}', possible_number_pattern='\\d{8}', example_number='80080002'),
     premium_rate=PhoneNumberDesc(national_number_pattern='(?:9(?:4[1568]|5[178]))\\d{5}', possible_number_pattern='\\d{8}', example_number='94515151'),
     shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
--- python/phonenumbers/shortdata/region_IN.py
+++ python/phonenumbers/shortdata/region_IN.py
@@ -6,7 +6,7 @@ PHONE_METADATA_IN = PhoneMetadata(id='IN', country_code=None, international_pref
     toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     premium_rate=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
     emergency=PhoneNumberDesc(national_number_pattern='1(?:0[0128]|12|298)|2611', possible_number_pattern='\\d{3,4}', example_number='108'),
-    short_code=PhoneNumberDesc(national_number_pattern='1(?:0[0128]|12|298)|2611|5(?:143[46]|3000|757555)', possible_number_pattern='\\d{3,7}', example_number='108'),
-    standard_rate=PhoneNumberDesc(national_number_pattern='5(?:143[46]|757555)', possible_number_pattern='\\d{5,7}', example_number='5757555'),
+    short_code=PhoneNumberDesc(national_number_pattern='1(?:0[0128]|12|298)|2611|5(?:14(?:2[5-9]|[34]\\d)|3000|757555)', possible_number_pattern='\\d{3,7}', example_number='108'),
+    standard_rate=PhoneNumberDesc(national_number_pattern='5(?:14(?:2[5-9]|[34]\\d)|757555)', possible_number_pattern='\\d{5,7}', example_number='5757555'),
     carrier_specific=PhoneNumberDesc(national_number_pattern='53000', possible_number_pattern='\\d{5}'),
     short_data=True)
