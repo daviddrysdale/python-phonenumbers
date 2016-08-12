3e1d0f7025b9f04e4b177c42ca829342ea2ee8a1
David Drysdale <dmd@lurklurk.org>
2016-08-12 17:22:21 +0100

    Prep for 7.5.2 release

--- python/phonenumbers/__init__.py
+++ python/phonenumbers/__init__.py
@@ -135,7 +135,7 @@ from .phonenumbermatcher import PhoneNumberMatch, PhoneNumberMatcher, Leniency
 
 # Version number is taken from the upstream libphonenumber version
 # together with an indication of the version of the Python-specific code.
-__version__ = "7.5.1"
+__version__ = "7.5.2"
 
 __all__ = ['PhoneNumber', 'CountryCodeSource', 'FrozenPhoneNumber',
            'REGION_CODE_FOR_NON_GEO_ENTITY', 'NumberFormat', 'PhoneNumberDesc', 'PhoneMetadata',
