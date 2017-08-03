f955b26facff6529d09a410cebebf92457aafdfa
David Drysdale <drysdale@google.com>
2017-08-03 18:37:37 +0100

    Generated files for metadata

--- python/phonenumbers/shortdata/region_IN.py
+++ python/phonenumbers/shortdata/region_IN.py
@@ -2,12 +2,12 @@
 from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_IN = PhoneMetadata(id='IN', country_code=None, international_prefix=None,
-    general_desc=PhoneNumberDesc(national_number_pattern='[125]\\d{2,6}', possible_length=(3, 4, 5, 6, 7)),
+    general_desc=PhoneNumberDesc(national_number_pattern='[125]\\d{2,7}', possible_length=(3, 4, 5, 6, 7, 8)),
     toll_free=PhoneNumberDesc(national_number_pattern='1\\d{2,5}', example_number='105010', possible_length=(3, 4, 5, 6)),
-    premium_rate=PhoneNumberDesc(national_number_pattern='11[67][0-2]\\d{3}', example_number='1160530', possible_length=(7,)),
+    premium_rate=PhoneNumberDesc(national_number_pattern='11[67][0-2]\\d{3}|56161561', example_number='1160530', possible_length=(7, 8)),
     emergency=PhoneNumberDesc(national_number_pattern='1(?:0[0128]|12|298)|2611', example_number='108', possible_length=(3, 4)),
-    short_code=PhoneNumberDesc(national_number_pattern='1(?:0(?:[01248]|5(?:010|6|902)|7(?:[07]|80)|9[0157])|1(?:[289]|[67][0-2]\\d{3})|2(?:1|98)|39|4[01]|55[23]\\d{2}|7000|9(?:0[019]|47|50|6[1347]|[89]))|2611|5(?:14(?:2[5-9]|[34]\\d)|3000|757555)', example_number='108', possible_length=(3, 4, 5, 6, 7)),
+    short_code=PhoneNumberDesc(national_number_pattern='1(?:0(?:[01248]|5(?:010|6|902)|7(?:[07]|80)|9[0157])|1(?:[289]|[67][0-2]\\d{3})|2(?:1|98)|39|4[01]|55[23]\\d{2}|7000|9(?:0[019]|47|50|6[1347]|[89]))|2611|5(?:14(?:2[5-9]|[34]\\d)|3000|6161(?:17[89]|561)|757555)', example_number='108', possible_length=(3, 4, 5, 6, 7, 8)),
     standard_rate=PhoneNumberDesc(national_number_pattern='5(?:14(?:2[5-9]|[34]\\d)|757555)', example_number='5757555', possible_length=(5, 7)),
-    carrier_specific=PhoneNumberDesc(national_number_pattern='1(?:1(?:[67][0-2]\\d{3}|[89])|21|4[01]|55330|7\\d{3}|9(?:[89]|09))|53000', example_number='53000', possible_length=(3, 4, 5, 6, 7)),
-    sms_services=PhoneNumberDesc(national_number_pattern='1(?:39|90[019])|5(?:14(?:2[5-9]|[34]\\d)|757555)', example_number='51431', possible_length=(3, 4, 5, 7)),
+    carrier_specific=PhoneNumberDesc(national_number_pattern='1(?:1(?:[67][0-2]\\d{3}|[89])|21|4[01]|55330|7\\d{3}|9(?:[89]|09))|5(?:3000|6161(?:17[89]|561))', example_number='53000', possible_length=(3, 4, 5, 6, 7, 8)),
+    sms_services=PhoneNumberDesc(national_number_pattern='1(?:39|90[019])|5(?:14(?:2[5-9]|[34]\\d)|6161(?:17[89]|561)|757555)', example_number='51431', possible_length=(3, 4, 5, 7, 8)),
     short_data=True)
--- python/phonenumbers/shortdata/region_KW.py
+++ python/phonenumbers/shortdata/region_KW.py
@@ -4,6 +4,6 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 PHONE_METADATA_KW = PhoneMetadata(id='KW', country_code=None, international_prefix=None,
     general_desc=PhoneNumberDesc(national_number_pattern='[18]\\d{2,4}', possible_length=(3, 5)),
     emergency=PhoneNumberDesc(national_number_pattern='112', example_number='112', possible_length=(3,)),
-    short_code=PhoneNumberDesc(national_number_pattern='1\\d{2}|89887', example_number='177', possible_length=(3, 5)),
+    short_code=PhoneNumberDesc(national_number_pattern='1[0-7]\\d|89887', example_number='177', possible_length=(3, 5)),
     carrier_specific=PhoneNumberDesc(national_number_pattern='89887', example_number='89887', possible_length=(5,)),
     short_data=True)
