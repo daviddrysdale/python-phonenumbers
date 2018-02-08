1a07566f482342bdc02677275e83877487bd7fc3
David Drysdale <drysdale@google.com>
2018-02-08 16:21:53 +0000

    Generated files for metadata

--- python/phonenumbers/data/region_HK.py
+++ python/phonenumbers/data/region_HK.py
@@ -3,7 +3,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_HK = PhoneMetadata(id='HK', country_code=852, international_prefix='00(?:[126-9]|30|5[09])?',
     general_desc=PhoneNumberDesc(national_number_pattern='[2-7]\\d{7}|8[0-3]\\d{6,7}|9\\d{4,10}', possible_length=(5, 6, 7, 8, 9, 11)),
-    fixed_line=PhoneNumberDesc(national_number_pattern='(?:2(?:[13-8]\\d|2[013-9]|9[0-24-9])|3(?:[1569][0-24-9]|4[0-246-9]|7[0-24-69]|89)|58[01])\\d{5}', example_number='21234567', possible_length=(8,)),
+    fixed_line=PhoneNumberDesc(national_number_pattern='(?:2(?:[13-8]\\d|2[013-9]|9[0-24-9])\\d|3(?:[1569][0-24-9]\\d|4[0-246-9]\\d|7[0-24-69]\\d|8(?:40|9\\d))|58(?:0[1-8]|1[2-9]))\\d{4}', example_number='21234567', possible_length=(8,)),
     mobile=PhoneNumberDesc(national_number_pattern='(?:46(?:0[0-6]|4[07-9])|5(?:[1-59][0-46-9]\\d|6[0-4689]\\d|7(?:[0-2469]\\d|30))|6(?:0[1-9]\\d|[1459]\\d{2}|2(?:[0-57-9]\\d|6[01])|[368][0-57-9]\\d|7[0-79]\\d)|9(?:0[1-9]\\d|1[02-9]\\d|2(?:[0-8]\\d|9[03-9])|[358][0-8]\\d|[467]\\d{2}))\\d{4}', example_number='51234567', possible_length=(8,)),
     toll_free=PhoneNumberDesc(national_number_pattern='800\\d{6}', example_number='800123456', possible_length=(9,)),
     premium_rate=PhoneNumberDesc(national_number_pattern='900(?:[0-24-9]\\d{7}|3\\d{1,4})', example_number='90012345678', possible_length=(5, 6, 7, 8, 11)),
--- python/phonenumbers/shortdata/region_ZA.py
+++ python/phonenumbers/shortdata/region_ZA.py
@@ -6,7 +6,7 @@ PHONE_METADATA_ZA = PhoneMetadata(id='ZA', country_code=None, international_pref
     premium_rate=PhoneNumberDesc(national_number_pattern='41(?:348|851)', example_number='41851', possible_length=(5,)),
     emergency=PhoneNumberDesc(national_number_pattern='1(?:0(?:1(?:11|77)|7)|12)', example_number='10111', possible_length=(3, 5)),
     short_code=PhoneNumberDesc(national_number_pattern='1(?:0(?:1(?:11|77)|20|7)|1[12]|77(?:3[237]|[45]7|6[279]|9[26]))|[34]\\d{4}', example_number='10111', possible_length=(3, 4, 5)),
-    standard_rate=PhoneNumberDesc(national_number_pattern='3(?:078[23]|7(?:064|567)|8126)|4(?:3(?:699|946)|7751)', example_number='47751', possible_length=(5,)),
+    standard_rate=PhoneNumberDesc(national_number_pattern='3(?:078[23]|7(?:064|567)|8126)|4(?:2699|3(?:699|94[16])|7751|8837)', example_number='47751', possible_length=(5,)),
     carrier_specific=PhoneNumberDesc(national_number_pattern='1(?:07|11)', example_number='111', possible_length=(3,)),
     sms_services=PhoneNumberDesc(national_number_pattern='[34]\\d{4}', example_number='47751', possible_length=(5,)),
     short_data=True)
