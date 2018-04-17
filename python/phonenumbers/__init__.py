c47a71f8965b9712e7d0a3d189f69d227065562a
David Drysdale <drysdale@google.com>
2018-04-17 17:30:28 +0100

    Prep for 8.9.4 release

--- python/phonenumbers/__init__.py
+++ python/phonenumbers/__init__.py
@@ -147,7 +147,7 @@ from .phonenumbermatcher import PhoneNumberMatch, PhoneNumberMatcher, Leniency
 
 # Version number is taken from the upstream libphonenumber version
 # together with an indication of the version of the Python-specific code.
-__version__ = "8.9.3"
+__version__ = "8.9.4"
 
 __all__ = ['PhoneNumber', 'CountryCodeSource', 'FrozenPhoneNumber',
            'REGION_CODE_FOR_NON_GEO_ENTITY', 'NumberFormat', 'PhoneNumberDesc', 'PhoneMetadata',
