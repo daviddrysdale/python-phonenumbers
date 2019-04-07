e4fae61d2fd85c842e7fa98bc8695d3c52499a25
Jon Dufresne <jon.dufresne@gmail.com>
2019-04-07 09:50:17 -0700

    Prefer setuptools over distutils in setup.py
    setuptools is available on modern Python environments and allows for
    newer features such as the python_requires argument.

--- setup.py
+++ setup.py
@@ -15,14 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import distutils.core
+from setuptools import setup
 import sys
-# Importing setuptools adds some features like "setup.py test", but
-# it's optional so swallow the error if it's not there.
-try:
-    import setuptools
-except ImportError:
-    pass
 
 if sys.version_info < (2, 7):
     raise RuntimeError("Python 2.7 or newer is required")
@@ -33,31 +27,36 @@ sys.path.append('python')
 # Discover version of phonenumbers package
 from phonenumbers import __version__
 
-distutils.core.setup(name='phonenumbers',
-                     version=__version__,
-                     description="Python version of Google's common library for parsing, formatting, storing and validating international phone numbers.",
-                     author='David Drysdale',
-                     author_email='dmd@lurklurk.org',
-                     url='https://github.com/daviddrysdale/python-phonenumbers',
-                     license='Apache License 2.0',
-                     packages=['phonenumbers', 'phonenumbers.data', 'phonenumbers.geodata', 'phonenumbers.shortdata',
-                               'phonenumbers.carrierdata', 'phonenumbers.tzdata'],
-                     package_dir={'': 'python'},
-                     test_suite="tests.examplenumberstest",
-                     platforms='Posix; MacOS X; Windows',
-                     classifiers=['Development Status :: 5 - Production/Stable',
-                                  'Intended Audience :: Developers',
-                                  'License :: OSI Approved :: Apache Software License',
-                                  'Operating System :: OS Independent',
-                                  'Topic :: Communications :: Telephony',
-                                  'Programming Language :: Python :: 2',
-                                  'Programming Language :: Python :: 2.7',
-                                  'Programming Language :: Python :: 3',
-                                  'Programming Language :: Python :: 3.4',
-                                  'Programming Language :: Python :: 3.5',
-                                  'Programming Language :: Python :: 3.6',
-                                  'Programming Language :: Python :: 3.7',
-                                  'Programming Language :: Python :: Implementation :: CPython',
-                                  'Programming Language :: Python :: Implementation :: PyPy',
-                                  ],
-                     )
+setup(
+    name='phonenumbers',
+    version=__version__,
+    description="Python version of Google's common library for parsing, formatting, storing and validating international phone numbers.",
+    author='David Drysdale',
+    author_email='dmd@lurklurk.org',
+    url='https://github.com/daviddrysdale/python-phonenumbers',
+    license='Apache License 2.0',
+    packages=[
+        'phonenumbers', 'phonenumbers.data', 'phonenumbers.geodata', 'phonenumbers.shortdata',
+        'phonenumbers.carrierdata', 'phonenumbers.tzdata',
+    ],
+    package_dir={'': 'python'},
+    test_suite="tests.examplenumberstest",
+    platforms='Posix; MacOS X; Windows',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Communications :: Telephony',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+)
