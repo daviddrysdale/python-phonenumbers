2ac7ccfb0e01b7c72388afb9bc3216dfb180614c
David Drysdale <drysdale@google.com>
2020-07-25 09:10:05 +0100

    Prep for 8.12.7 release

--- python/phonenumbers/__init__.py
+++ python/phonenumbers/__init__.py
@@ -146,7 +146,7 @@ from .phonenumbermatcher import PhoneNumberMatch, PhoneNumberMatcher, Leniency
 
 # Version number is taken from the upstream libphonenumber version
 # together with an indication of the version of the Python-specific code.
-__version__ = "8.12.6"
+__version__ = "8.12.7"
 
 __all__ = ['PhoneNumber', 'CountryCodeSource', 'FrozenPhoneNumber',
            'REGION_CODE_FOR_NON_GEO_ENTITY', 'NumberFormat', 'PhoneNumberDesc', 'PhoneMetadata',
