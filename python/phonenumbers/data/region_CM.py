00222a0a5c0fb7a3b1ad3cabb4aefbc51a31c819
David Drysdale <drysdale@google.com>
2020-10-10 10:50:03 +0100

    Generated files for metadata

--- python/phonenumbers/data/region_CM.py
+++ python/phonenumbers/data/region_CM.py
@@ -7,4 +7,5 @@ PHONE_METADATA_CM = PhoneMetadata(id='CM', country_code=237, international_prefi
     mobile=PhoneNumberDesc(national_number_pattern='(?:24[23]|6[5-9]\\d)\\d{6}', example_number='671234567', possible_length=(9,)),
     toll_free=PhoneNumberDesc(national_number_pattern='88\\d{6}', example_number='88012345', possible_length=(8,)),
     number_format=[NumberFormat(pattern='(\\d{2})(\\d{2})(\\d{2})(\\d{2})', format='\\1 \\2 \\3 \\4', leading_digits_pattern=['88']),
-        NumberFormat(pattern='(\\d)(\\d{2})(\\d{2})(\\d{2})(\\d{2})', format='\\1 \\2 \\3 \\4 \\5', leading_digits_pattern=['[26]'])])
+        NumberFormat(pattern='(\\d)(\\d{2})(\\d{2})(\\d{2})(\\d{2})', format='\\1 \\2 \\3 \\4 \\5', leading_digits_pattern=['[26]'])],
+    mobile_number_portable_region=True)
