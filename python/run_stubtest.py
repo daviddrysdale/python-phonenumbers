66e37561192f69cbd65a343c4f5735daa2dabc37
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-01 21:40:20 +0100

    👷 Add `run_stubtest.py`

---
+++ python/run_stubtest.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import tempfile
+
+from mypy import stubtest
+
+if __name__ == '__main__':
+    file_descriptor, file_name = tempfile.mkstemp(text=True)
+    excluded = "\n".join(["phonenumbers.pb2.*"])  # exclude pb2/ (whitelist errors)
+    with os.fdopen(file_descriptor, "w") as f:
+        f.write(excluded)
+    ret = stubtest.test_stubs(stubtest.parse_options(["phonenumbers", "--whitelist", file_name]))
+    os.remove(file_name)
+    sys.exit(ret)
