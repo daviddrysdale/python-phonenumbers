7e754c0956af016255f48ea8a6f80705167f06af
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-02 07:52:44 +0100

    ♻️ Use `tempfile.NamedTemporaryFile`

--- python/run_stubtest.py
+++ python/run_stubtest.py
@@ -5,10 +5,9 @@ import tempfile
 from mypy import stubtest
 
 if __name__ == "__main__":
-    file_descriptor, file_name = tempfile.mkstemp(text=True)
     excluded = "\n".join(["phonenumbers.pb2.*"])  # exclude pb2/ (whitelist errors)
-    with os.fdopen(file_descriptor, "w") as f:
-        f.write(excluded)
-    ret = stubtest.test_stubs(stubtest.parse_options(["phonenumbers", "--whitelist", file_name]))
-    os.remove(file_name)
+    with tempfile.NamedTemporaryFile("w+", delete=False) as file:
+        file.write(excluded)
+    ret = stubtest.test_stubs(stubtest.parse_options(["phonenumbers", "--whitelist", file.name]))
+    os.remove(file.name)
     sys.exit(ret)
