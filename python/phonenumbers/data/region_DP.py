57819bd99acdba864dd9cf91518e65f7555e668a
Rajesh Panicker <rajesh@dialpad.com>
2021-09-13 23:10:33 -0700

    Fixing the Unit Tests for Region Dialpadistan

--- python/phonenumbers/data/region_DP.py
+++ python/phonenumbers/data/region_DP.py
@@ -2,7 +2,7 @@
 from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_DP = PhoneMetadata(id='DP', country_code=803, international_prefix=None,
-    general_desc=PhoneNumberDesc(national_number_pattern='\\d{10}', possible_length=(10,)),
-    voip=PhoneNumberDesc(national_number_pattern='\\d{10}', possible_length=(10,)),
+    general_desc=PhoneNumberDesc(national_number_pattern='\\d{10}', example_number='1000000141', possible_length=(10,)),
+    voip=PhoneNumberDesc(national_number_pattern='\\d{10}', example_number='1000000141', possible_length=(10,)),
     number_format=[NumberFormat(pattern='(\\d{3})(\\d{7})', format='\\1\\2')],
     leading_zero_possible=False)
