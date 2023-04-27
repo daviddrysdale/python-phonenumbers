59f0ef68779de40d63c865a67ec1a32e734a913a
David Drysdale <drysdale@google.com>
2023-04-27 13:19:21 +0100

    Generated files for metadata

--- python/phonenumbers/data/region_GF.py
+++ python/phonenumbers/data/region_GF.py
@@ -3,10 +3,10 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_GF = PhoneMetadata(id='GF', country_code=594, international_prefix='00',
     general_desc=PhoneNumberDesc(national_number_pattern='[56]94\\d{6}|(?:80|9\\d)\\d{7}', possible_length=(9,)),
-    fixed_line=PhoneNumberDesc(national_number_pattern='594(?:[0239]\\d|1[0-2]|4[03-9]|5[6-9]|6[0-3]|80)\\d{4}', example_number='594101234', possible_length=(9,)),
+    fixed_line=PhoneNumberDesc(national_number_pattern='594(?:[0239]\\d|[16][0-3]|4[03-9]|5[6-9]|80)\\d{4}', example_number='594101234', possible_length=(9,)),
     mobile=PhoneNumberDesc(national_number_pattern='694(?:[0-249]\\d|3[0-8])\\d{4}', example_number='694201234', possible_length=(9,)),
     toll_free=PhoneNumberDesc(national_number_pattern='80[0-5]\\d{6}', example_number='800012345', possible_length=(9,)),
-    voip=PhoneNumberDesc(national_number_pattern='9(?:(?:396|76\\d)\\d|476[01])\\d{4}', example_number='976012345', possible_length=(9,)),
+    voip=PhoneNumberDesc(national_number_pattern='9(?:(?:396|76\\d)\\d|476[0-2])\\d{4}', example_number='976012345', possible_length=(9,)),
     national_prefix='0',
     national_prefix_for_parsing='0',
     number_format=[NumberFormat(pattern='(\\d{3})(\\d{2})(\\d{2})(\\d{2})', format='\\1 \\2 \\3 \\4', leading_digits_pattern=['[56]|9[47]'], national_prefix_formatting_rule='0\\1'),
--- python/phonenumbers/data/region_YT.py
+++ python/phonenumbers/data/region_YT.py
@@ -2,9 +2,9 @@
 from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 
 PHONE_METADATA_YT = PhoneMetadata(id='YT', country_code=262, international_prefix='00',
-    general_desc=PhoneNumberDesc(national_number_pattern='(?:(?:(?:26|63)9|80\\d)\\d\\d|93980)\\d{4}', possible_length=(9,)),
+    general_desc=PhoneNumberDesc(national_number_pattern='(?:(?:(?:26|63)9|80\\d)\\d|9398)\\d{5}', possible_length=(9,)),
     fixed_line=PhoneNumberDesc(national_number_pattern='269(?:0[0-467]|5[0-3]|6\\d|[78]0)\\d{4}', example_number='269601234', possible_length=(9,)),
-    mobile=PhoneNumberDesc(national_number_pattern='(?:639(?:0[0-79]|1[019]|[267]\\d|3[09]|40|5[05-9]|9[04-79])|93980)\\d{4}', example_number='639012345', possible_length=(9,)),
+    mobile=PhoneNumberDesc(national_number_pattern='(?:639(?:0[0-79]|1[019]|[267]\\d|3[09]|40|5[05-9]|9[04-79])|9398[01])\\d{4}', example_number='639012345', possible_length=(9,)),
     toll_free=PhoneNumberDesc(national_number_pattern='80\\d{7}', example_number='801234567', possible_length=(9,)),
     national_prefix='0',
     national_prefix_for_parsing='0',
