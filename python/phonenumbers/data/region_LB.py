8e275f186f29f4c5afa41a49e187080824c5b870
Mostafa Nawam <mynawam@bluemint.ai>
2025-10-15 10:44:09 +0300

    handle the lebanese +961 790*****

--- python/phonenumbers/data/region_LB.py
+++ python/phonenumbers/data/region_LB.py
@@ -4,7 +4,7 @@ from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
 PHONE_METADATA_LB = PhoneMetadata(id='LB', country_code=961, international_prefix='00',
     general_desc=PhoneNumberDesc(national_number_pattern='[27-9]\\d{7}|[13-9]\\d{6}', possible_length=(7, 8)),
     fixed_line=PhoneNumberDesc(national_number_pattern='7(?:62|8[0-6]|9[04-9])\\d{4}|(?:[14-69]\\d|2(?:[14-69]\\d|[78][1-9])|7[2-57]|8[02-9])\\d{5}', example_number='1123456', possible_length=(7, 8)),
-    mobile=PhoneNumberDesc(national_number_pattern='(?:(?:3|81)\\d|7(?:[01]\\d|6[013-9]|8[7-9]|9[1-3]))\\d{5}', example_number='71123456', possible_length=(7, 8)),
+    mobile=PhoneNumberDesc(national_number_pattern='(?:(?:3|81)\\d|7(?:[01]\\d|6[013-9]|8[7-9]|9\\d))\\d{5}', example_number='71123456', possible_length=(7, 8)),
     premium_rate=PhoneNumberDesc(national_number_pattern='9[01]\\d{6}', example_number='90123456', possible_length=(8,)),
     shared_cost=PhoneNumberDesc(national_number_pattern='80\\d{6}', example_number='80123456', possible_length=(8,)),
     national_prefix='0',
